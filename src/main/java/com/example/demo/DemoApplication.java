fd49243752010f0b7ec7d23e285793f3d787a283
lihongquan0 <lihongquan0@gmail.com>
2020-02-22 20:36:19 +0800

    test01 of Demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// test to git 
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
